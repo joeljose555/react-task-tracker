a0f7129c5abdad6560d32788f7bb6e60cc822cc9
Joel Jose <joeljose555@gmail.com>
2021-08-20 18:07:14 +0530

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import Button from "./Button"
+const Header = ({title , onAdd, showAdd}) => {
+    return (
+        <header className = "header">
+            <h1>{title}</h1>
+            <Button color={showAdd ? 'red' : 'green'} text={showAdd ? 'close' : 'add'} onClick={onAdd}/>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title:'Task Tracker',
+}
+export default Header
